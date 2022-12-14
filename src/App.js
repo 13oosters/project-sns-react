415433b9e85c102d14ed9c919212d6b6bd7c0106
h12j21-star <jh06079@naver.com>
2022-12-14 18:44:50 +0900

    Revert "design:  App.js파일 수정 #2"  reverts commit dd1e83117ea8b5211358e55eda4f0eb570341694.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import LoginPage from "./pages/LoginPage";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
