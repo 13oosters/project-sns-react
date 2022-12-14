5029c9566dbb3f8203b0c8aba5d86abd5941a9bc
junyub.lee <dlwnsduq23@gmail.com>
2022-12-14 18:25:42 +0900

    design: 로고컴포넌트 제작 #1

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -1,5 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+import LogoImage from "../../assets/image/logo-login.png";
+import SplashLogoImage from "../../assets/image/logo-splash.png";
 
-export default function Logo() {
-  return <img alt="멍하냥" />;
+const LogoImg = styled.img`
+  margin-top: ${(props) => props.marginTop}rem;
+  transform: translateX(-4%);
+`;
+
+export default function Logo(props) {
+  if(props.name === "login") {
+    return <LogoImg src={LogoImage} marginTop={7.3}/>
+  } else if (props.name === "splash"){
+    return <LogoImg src={SplashLogoImage} marginTop={20}/>
+  }
 }
