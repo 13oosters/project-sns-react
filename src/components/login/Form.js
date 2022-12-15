3c76fb92a06278ef1afebc6425a932dd783e78a1
h12j21-star <jh06079@naver.com>
2022-12-16 00:24:51 +0900

    design: Form 스타일드 컴포넌트 제작 #2

--- src/components/login/Form.js
+++ src/components/login/Form.js
@@ -1,31 +1,59 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "../style/Button";
+
+const TitleH2 = styled.h2`
+  margin-top: 3rem;
+  font-size: 2.4rem;
+  font-weight: ${(props) => props.theme.mediumFontWeight};
+`;
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 4rem;
+  text-align: left;
+`;
+const LoginInput = styled.input`
+  display: block;
+  width: 100%;
+  border-bottom: 0.1rem solid ${(props) => props.theme.lightColor};
+  padding: 0.8rem 0;
+  &::placeholder {
+    color: ${(props) => props.theme.lightColor};
+    font-size: ${(props) => props.theme.baseFontSize};
+  }
+`;
 
 export default function Form({ title, buttonText }) {
   return (
     <>
-      <h2>{title}</h2>
-      <form method="post" action="#;">
+      <TitleH2>{title}</TitleH2>
+      <LoginForm method="post" action="#;">
         <label>
           이메일
-          <input
+          <LoginInput
             type="email"
             required
             placeholder="이메일 주소를 입력해주세요"
           />
         </label>
-        <label>
+        <label style={{ marginTop: "1.6rem" }}>
           비밀번호
-          <input
+          <LoginInput
             type="password"
             required
             placeholder="비밀번호를 설정해 주세요"
           />
         </label>
-        <button type="submit">{buttonText}</button>
-      </form>
-      {/* 이준근이 작성한 함수 */}
-      {title === "로그인" ? <a href="#;">이메일로 회원가입</a> : null}
-      {/* yubmun 작업 진행했습니다 */}
+        <Button type="submit" disable>
+          {buttonText}
+        </Button>
+      </LoginForm>
+      {title === "로그인" ? (
+        <Button type="button" cancel>
+          이메일로 회원가입
+        </Button>
+      ) : null}
     </>
   );
 }
