78acff25efd8b9492a4e1580ca66f30718884a0a
h12j21-star <jh06079@naver.com>
2022-12-16 00:21:33 +0900

    design: theme 색상, 폰트사이즈 추가 #2

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -2,5 +2,5 @@ import React from "react";
 import logoImage from "../../assets/image/logo-login.png";
 
 export default function Logo() {
-  return <img src={logoImage} alt="멍하냥" />;
+  return <img style={{ marginLeft: "-1.3rem" }} src={logoImage} alt="멍하냥" />;
 }
