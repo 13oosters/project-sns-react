13bf041e82530b4b8c5bb22b43bef015b80dfd91
Jxxunnn <roof2586@naver.com>
2022-12-15 11:36:58 +0900

    design: 로그인 컴포넌트 첫 번째 화면 구현 #2 Co-authored-by: h12j21-star <h12j21-star@users.noreply.github.com>

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -5,48 +5,50 @@ import Button from "../style/Button";
 import kakaoLogo from "../../assets/image/logo-kakaotalk.png";
 import googleLogo from "../../assets/image/logo-google.png";
 
+const SecondaryColorDiv = styled.div`
+  border-top-left-radius: 4rem;
+  border-top-right-radius: 4rem;
+  padding: 6rem 3.4rem 8.2rem;
+  background-color: ${(props) => props.theme.sideColor};
+`;
+
 const LoginButton = styled(Button)`
   display: block;
   font-size: ${(props) => props.theme.baseFontSize};
   color: #000000;
-  background-color: #ffffff;
   background: ${(props) => `url(${props.image})`} no-repeat 15px center;
   border: 1px solid #c5985e;
+  background-color: #ffffff;
 `;
 
 export default function Login() {
   return (
-    <>
-      {/* 로그인 디자인 작업2 */}
-      {/* 로그인작업3*/}
-
-      {/* yumbun 2일차 작업 진행 (두번째 체크리스트 작업)*/}
+    <SecondaryColorDiv>
       <ul>
-        {/* 준근 기능 추가 2 */}
-        <li>
+        <li style={{ marginBottom: "2.5rem" }}>
           <LoginButton image={kakaoLogo} as="a" href="#;">
             카카오 계정으로 로그인
           </LoginButton>
         </li>
-        <li>
+        <li style={{ marginBottom: "4.9rem" }}>
           <LoginButton image={googleLogo} as="a" href="#;">
             구글 계정으로 로그인
           </LoginButton>
         </li>
       </ul>
-      <ul>
-        <li>
-          <a href="#;">이메일로 로그인</a>
+      <ul style={{ display: "flex", justifyContent: "center" }}>
+        <li style={{ marginRight: "1.2rem" }}>
+          <a style={{ display: "flex" }} href="#;">
+            <span>이메일로 로그인</span>
+            <span style={{ marginLeft: "1.2rem", marginTop: "-0.1rem" }}>
+              |
+            </span>
+          </a>
         </li>
         <li>
           <a href="#;">회원가입</a>
         </li>
       </ul>
-      {/* yubmun 2일차 작업 진행 (첫번째 체크리스트 작업) */}
-    </>
+    </SecondaryColorDiv>
   );
 }
-// 소영 테스트
-// 소영 테스트 2
-// 소영 테스트 3
-// 소영 테스트 4
---
+++ src/components/login/Welcome.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+import Logo from "../common/Logo";
+import Login from "./Login";
+// 73 26 94 13
+const PrimaryColorDiv = styled.div`
+  padding: 7.3rem 0 0 0;
+  background-color: ${(props) => props.theme.primaryColor};
+`;
+
+export default function Welcome() {
+  return (
+    <PrimaryColorDiv>
+      <Logo />
+      <Login />
+    </PrimaryColorDiv>
+  );
+}
