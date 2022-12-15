de0e995f9c4a5f5708119418dc60dd407b4e90f3
h12j21-star <jh06079@naver.com>
2022-12-16 00:18:51 +0900

    design: Welcome컴포넌트 제작 #2

---
+++ src/components/login/Welcome.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+import Logo from "../common/Logo";
+import Login from "./Login";
+// 73 26 94 13
+const PrimaryColorDiv = styled.div`
+  padding: 7.3rem 0 0 0;
+  background-color: ${(props) => props.theme.primaryColor};
+`;
+
+export default function Welcome({ setIsLoginClicked, setIsSignUpClicked }) {
+  return (
+    <PrimaryColorDiv>
+      <Logo />
+      <Login
+        setIsLoginClicked={setIsLoginClicked}
+        setIsSignupClicked={setIsSignUpClicked}
+      />
+    </PrimaryColorDiv>
+  );
+}
