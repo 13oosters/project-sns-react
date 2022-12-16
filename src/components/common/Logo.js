f519d422ebbc9fccc80f57ff00a575326395e99e
h12j21-star <jh06079@naver.com>
2022-12-16 12:08:31 +0900

    design: Logo마진값 설정 #2

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -1,6 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 import logoImage from "../../assets/image/logo-login.png";
 
+const LogoImage = styled.img`
+  margin-bottom: 9.4rem;
+`;
+
 export default function Logo() {
-  return <img src={logoImage} alt="멍하냥" />;
+  return <LogoImage src={logoImage} alt="멍하냥" />;
 }
