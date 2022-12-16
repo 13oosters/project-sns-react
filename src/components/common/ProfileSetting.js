1c27322d5a50a07c1d21974338c7858a53c67bd7
yubmun <dlwnsduq23@gmail.com>
2022-12-16 13:20:55 +0900

    feat: 프로필 설정 라우팅 처리 #1

--- src/components/common/ProfileSetting.js
+++ src/components/common/ProfileSetting.js
@@ -1,18 +1,55 @@
 import React from "react";
+import styled from "styled-components";
+import { FormH2 } from "../login/Form";
+import LayoutSection from "../style/PageLayout";
+import basicProfileImage from "../../assets/image/basic-profile-img.png";
+import profileUploadButton from "../../assets/image/profile-upload-button.png";
+
+const ProfileSettingH2 = styled(FormH2)`
+  margin-bottom: 1.2rem;
+`
+const ProfileSettingP = styled.p`
+  color: ${(props) => props.theme.textColor};
+  font-size: ${(props) => props.theme.baseFontSize};
+  margin-bottom: 3rem;
+`
+const ProfileImageDiv = styled.div`
+  position: relative;
+`
+const ProfileImageLabel = styled.label`
+  position: absolute;
+  cursor: pointer;
+  width: 3.6rem;
+  height: 3.6rem;
+  bottom: 0;
+  right: 33%;
+  background: url(${profileUploadButton}) no-repeat;
+`
+
+const ProfileImageInput = styled.input`
+  position: absolute;
+  width: 0;
+  height: 0;
+  padding: 0;
+  overflow: hidden;
+  border: 0;
+`
 
 export default function ProfileSetting({ title }) {
   return (
     <>
+    <LayoutSection paddingValue={3.4} backgroundColor={"#ffffff"}>
       {title === "프로필 설정" ? (
         <>
-          <h2>{title}</h2>
-          <p>나중에 언제든지 변경할 수 있습니다.</p>
+          <ProfileSettingH2>{title}</ProfileSettingH2>
+          <ProfileSettingP>나중에 언제든지 변경할 수 있습니다.</ProfileSettingP>
         </>
       ) : null}
-      <div>
-        <input type="file" />
-        <img src="" alt="넣어주세요" />
-      </div>
+      <ProfileImageDiv>
+        <img src={basicProfileImage} alt="넣어주세요" />
+        <ProfileImageInput id="file" type="file" />
+        <ProfileImageLabel htmlFor="file"></ProfileImageLabel>
+      </ProfileImageDiv>
       <form method="post" action="#;">
         <label>
           사용자 이름
@@ -35,6 +72,7 @@ export default function ProfileSetting({ title }) {
         </label>
         <a href="#;">{title === "프로필 설정" ? "멍하냥 시작하기" : "저장"}</a>
       </form>
+    </LayoutSection>
     </>
   );
 }
--- src/components/login/Form.js
+++ src/components/login/Form.js
@@ -1,9 +1,10 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Button from "../style/Button";
 import LayoutSection from "../style/PageLayout";
 
-const FormH2 = styled.h2`
+export const FormH2 = styled.h2`
   font-size: ${(props) => props.theme.xxLargeFontSize};
   font-weight: 500;
   margin-top: 3rem;
@@ -27,47 +28,96 @@ const LoginFormInput = styled.input`
   width: 100%;
   padding: 0.9rem 0;
   border-bottom: 1px solid #dbdbdb;
-  &:focus{
+  &:focus {
     outline: none;
     border-bottom: 1px solid ${(props) => props.theme.primaryColor};
   }
-`
+`;
 const LoginFormButton = styled(Button)`
   margin-top: 1.4rem;
   margin-bottom: 2rem;
-`
+`;
 const SignUpButton = styled.button`
   color: #767676;
   font-size: 1.2rem;
   font-weight: 400;
-`
+`;
 
 export default function Form({ title, buttonText }) {
-  return (
-    <>
-      <LayoutSection paddingValue={3.4} backgroundColor={"#ffffff"}>
-        <FormH2>{title}</FormH2>
-        <LoginForm method="post" action="#;">
-          <label>
-            <LoginFormP>이메일</LoginFormP>
-            <LoginFormInput
-              type="email"
-              required
-              placeholder="이메일 주소를 입력해주세요"
-            />
-          </label>
-          <label>
-            <LoginFormP>비밀번호</LoginFormP>
-            <LoginFormInput
-              type="password"
-              required
-              placeholder="비밀번호를 설정해 주세요"
-            />
-          </label>
-          <LoginFormButton>{buttonText}</LoginFormButton>
-        </LoginForm>
-        {title === "로그인" ? <SignUpButton type="button">이메일로 회원가입</SignUpButton> : null}
-      </LayoutSection>
-    </>
-  );
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  const navigate = useNavigate();
+
+  const clickNext = () => {
+    if(buttonText === "다음"){
+      navigate("/profile-setting");
+    }
+  }
+
+    return (
+      <>
+        <LayoutSection paddingValue={3.4} backgroundColor={"#ffffff"}>
+          <FormH2>{title}</FormH2>
+          <LoginForm onSubmit={handleSubmit(onSubmit)}>
+            <label>
+              <LoginFormP>이메일</LoginFormP>
+              {title === "로그인" ? (
+                <LoginFormInput
+                  type="email"
+                  required
+                  placeholder="이메일 주소를 입력해주세요"
+                  {...register("email", {
+                    required: "이메일은 필수 입력입니다.",
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: "이메일 형식에 맞지 않습니다.",
+                    },
+                  })}
+                />
+              ) : (
+                <LoginFormInput
+                  type="email"
+                  required
+                  placeholder="이메일 주소를 입력해주세요"
+                  {...register("email", {})}
+                />
+              )}
+              {errors.email && (
+                <small role="alert">{errors.email.message}</small>
+              )}
+            </label>
+            <label>
+              <LoginFormP>비밀번호</LoginFormP>
+              <LoginFormInput
+                type="password"
+                required
+                placeholder="비밀번호를 설정해 주세요"
+                {...register("password", {
+                  required: "비밀번호는 필수 입력입니다.",
+                  minLength: {
+                    value: 6,
+                    message: "비밀번호는 6자 이상이어야 합니다.",
+                  },
+                })}
+              />
+              {errors.email && (
+                <small role="alert">{errors.email.message}</small>
+              )}
+            </label>
+            <LoginFormButton onClick={clickNext}>{buttonText}</LoginFormButton>
+          </LoginForm>
+          {title === "로그인" ? (
+            <SignUpButton type="button" onClick={()=>{navigate('/sign-up')}} >
+              이메일로 회원가입
+            </SignUpButton>
+          ) : null}
+        </LayoutSection>
+      </>
+    );
 }
