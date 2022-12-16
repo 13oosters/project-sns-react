2611acb0189e7ce67ee631a7aec01cac074e0fff
yubmun <dlwnsduq23@gmail.com>
2022-12-16 16:40:17 +0900

    feat: 유효성 검사 진행중 #1

--- src/components/login/Form.js
+++ src/components/login/Form.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
@@ -11,20 +12,20 @@ export const FormH2 = styled.h2`
   margin-bottom: 4rem;
 `;
 
-const LoginForm = styled.form`
+export const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
   gap: 1.6rem;
 `;
 
-const LoginFormP = styled.p`
+export const LoginFormP = styled.p`
   color: #767676;
   font-size: 1.2rem;
   width: 100%;
   text-align: left;
 `;
 
-const LoginFormInput = styled.input`
+export const LoginFormInput = styled.input`
   width: 100%;
   padding: 0.9rem 0;
   border-bottom: 1px solid #dbdbdb;
@@ -32,13 +33,18 @@ const LoginFormInput = styled.input`
     outline: none;
     border-bottom: 1px solid ${(props) => props.theme.primaryColor};
   }
+  &::placeholder {
+    color: #dbdbdb;
+  }
 `;
-const LoginFormButton = styled(Button)`
+export const LoginFormButton = styled(Button)`
   margin-top: 1.4rem;
   margin-bottom: 2rem;
-`;
+  background-color: ${(props) => props.color};
+  `;
+
 const SignUpButton = styled.button`
-  color: #767676;
+  color: ${(props) => props.theme.textColor};
   font-size: 1.2rem;
   font-weight: 400;
 `;
@@ -46,78 +52,91 @@ const SignUpButton = styled.button`
 export default function Form({ title, buttonText }) {
   const {
     register,
-    handleSubmit,
-    // watch,
-    formState: { errors },
+    watch,
+    formState: { isSubmitting, errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
 
   const navigate = useNavigate();
 
   const clickNext = () => {
-    if(buttonText === "다음"){
+    if (buttonText === "다음") {
       navigate("/profile-setting");
     }
+  };
+
+  const [isValue, setIsValue] = useState(false);
+
+  console.log(isValue);
+
+  const checkIsValue = (e) => {
+    e.target.value && watch(e.target.type) ? setIsValue(true) : setIsValue(false);
   }
 
-    return (
-      <>
-        <LayoutSection paddingValue={3.4} backgroundColor={"#ffffff"}>
-          <FormH2>{title}</FormH2>
-          <LoginForm onSubmit={handleSubmit(onSubmit)}>
-            <label>
-              <LoginFormP>이메일</LoginFormP>
-              {title === "로그인" ? (
-                <LoginFormInput
-                  type="email"
-                  required
-                  placeholder="이메일 주소를 입력해주세요"
-                  {...register("email", {
-                    required: "이메일은 필수 입력입니다.",
-                    pattern: {
-                      value: /\S+@\S+\.\S+/,
-                      message: "이메일 형식에 맞지 않습니다.",
-                    },
-                  })}
-                />
-              ) : (
-                <LoginFormInput
-                  type="email"
-                  required
-                  placeholder="이메일 주소를 입력해주세요"
-                  {...register("email", {})}
-                />
-              )}
-              {errors.email && (
-                <small role="alert">{errors.email.message}</small>
-              )}
-            </label>
-            <label>
-              <LoginFormP>비밀번호</LoginFormP>
+
+
+
+
+  return (
+    <>
+      <LayoutSection paddingValue={3.4} backgroundColor={"#ffffff"}>
+        <FormH2>{title}</FormH2>
+        <LoginForm onSubmit={()=>{event.preventDefault();}}>
+          <label>
+            <LoginFormP>이메일</LoginFormP>
+            {title === "로그인" ? (
               <LoginFormInput
-                type="password"
+                type="email"
                 required
-                placeholder="비밀번호를 설정해 주세요"
-                {...register("password", {
-                  required: "비밀번호는 필수 입력입니다.",
-                  minLength: {
-                    value: 6,
-                    message: "비밀번호는 6자 이상이어야 합니다.",
+                placeholder="이메일 주소를 입력해주세요"
+                {...register("email", {
+                  required: "이메일은 필수 입력입니다.",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "이메일 형식에 맞지 않습니다.",
                   },
                 })}
+                onChange={checkIsValue}
               />
-              {errors.email && (
-                <small role="alert">{errors.email.message}</small>
-              )}
-            </label>
-            <LoginFormButton onClick={clickNext}>{buttonText}</LoginFormButton>
-          </LoginForm>
-          {title === "로그인" ? (
-            <SignUpButton type="button" onClick={()=>{navigate('/sign-up')}} >
-              이메일로 회원가입
-            </SignUpButton>
-          ) : null}
-        </LayoutSection>
-      </>
-    );
+            ) : (
+              <LoginFormInput
+                type="email"
+                required
+                placeholder="이메일 주소를 입력해주세요"
+                {...register("email", {})}
+              />
+            )}
+            {errors.email && <small role="alert">{errors.email.message}</small>}
+          </label>
+          <label>
+            <LoginFormP>비밀번호</LoginFormP>
+            <LoginFormInput
+              type="password"
+              required
+              placeholder="비밀번호를 설정해 주세요"
+              {...register("password", {
+                required: "비밀번호는 필수 입력입니다.",
+                pattern: {
+                  value: /^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,}$/i,
+                  message: "비밀번호 형식에 맞지 않습니다. 8자 이상이어야 합니다.",
+                },
+              })}
+              onChange={checkIsValue}
+            />
+            {errors.password && <small role="alert">{errors.password.message}</small>}
+          </label>
+          <LoginFormButton type="submit" disabled={isSubmitting} onClick={clickNext}>{buttonText}</LoginFormButton>
+        </LoginForm>
+        {title === "로그인" ? (
+          <SignUpButton
+            type="button"
+            onClick={() => {
+              navigate("/sign-up");
+            }}
+          >
+            이메일로 회원가입
+          </SignUpButton>
+        ) : null}
+      </LayoutSection>
+    </>
+  );
 }
