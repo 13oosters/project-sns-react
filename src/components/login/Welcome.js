1d8dd5c3c3a11fe8bb7f90b99dd820458cceb214
h12j21-star <jh06079@naver.com>
2022-12-16 12:09:15 +0900

    design: 이미지 가운데 정렬 #2

--- src/components/login/Welcome.js
+++ src/components/login/Welcome.js
@@ -5,7 +5,8 @@ import Logo from "../common/Logo";
 import Login from "./Login";
 // 73 26 94 13
 const PrimaryColorDiv = styled.div`
-  padding: 7.3rem 0 0 0;
+  padding: 7.3rem 0rem 0 0;
+  text-align: center;
   background-color: ${(props) => props.theme.primaryColor};
 `;
 
