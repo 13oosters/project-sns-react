759962bc9dcab5248ccf1d0adeafc8bba71ed640
h12j21-star <jh06079@naver.com>
2022-12-19 12:47:38 +0900

    design: profileSetting 기본 업로드 이미지 추가 #2
    Co-authored-by: leejungeun <Jxxunnn@users.noreply.github.com>

--- src/components/common/ProfileSetting.js
+++ src/components/common/ProfileSetting.js
@@ -5,16 +5,32 @@ import TitleH2 from "../style/form/TitleH2";
 import LoginForm from "../style/form/LoginForm";
 import LoginInput from "../style/form/LoginInput";
 import LoginButton from "../style/form/LoginButton";
-import UpLoadImage from "../src/assets/file-upload-button.png";
+import UpLoadImage from "../../../src/assets/image/profile-upload-button.png";
+import DefaultProfileImage from "../../../src/assets/image/basic-profile-img.png";
 
-const profileDiv = styled.div`
+const ProfileDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   &::after {
-    background: url({UpLoadImage});
+    content: "";
+    position: relative;
+    top: -30px;
+    right: -10%;
+    width: 3.6rem;
+    height: 3.6rem;
+    border-radius: 50%;
+    background: url(${UpLoadImage});
   }
 `;
+
+const ContentP = styled.p`
+  margin-top: 1.2rem;
+  text-align: center;
+  color: ${(props) => props.theme.darkLightColor};
+`;
+
+const UpLoadImg = styled.img``;
 
 export default function ProfileSetting({ title }) {
   return (
@@ -22,13 +38,15 @@ export default function ProfileSetting({ title }) {
       {title === "프로필 설정" ? (
         <>
           <TitleH2>{title}</TitleH2>
-          <p>나중에 언제든지 변경할 수 있습니다.</p>
+          <ContentP style={{ marginTop: "1.2rem" }}>
+            나중에 언제든지 변경할 수 있습니다.
+          </ContentP>
         </>
       ) : null}
-      <profileDiv>
+      <ProfileDiv>
         <input type="file" />
-        <img src="" alt="넣어주세요" />
-      </profileDiv>
+        <UpLoadImg src={DefaultProfileImage} alt="프로필 사진" />
+      </ProfileDiv>
       <LoginForm method="post" action="#;">
         <label>
           사용자 이름
@@ -38,7 +56,7 @@ export default function ProfileSetting({ title }) {
             placeholder="2~10자 이내여야 합니다."
           />
         </label>
-        <label>
+        <label style={{ marginTop: "1.6rem" }}>
           계정 ID
           <LoginInput
             type="text"
@@ -46,7 +64,7 @@ export default function ProfileSetting({ title }) {
             placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
           />
         </label>
-        <label>
+        <label style={{ marginTop: "1.6rem" }}>
           소개
           <LoginInput
             type="text"
