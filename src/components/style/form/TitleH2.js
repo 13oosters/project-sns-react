a86eac025eecba99b2d3c50989214c924fac9788
Jxxunnn <roof2586@naver.com>
2022-12-19 11:28:34 +0900

    design: form 요소 스타일드 컴포넌트 생성 #2

---
+++ src/components/style/form/TitleH2.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const TitleH2 = styled.h2`
+  margin-top: 3rem;
+  text-align: center;
+  font-size: 2.4rem;
+  font-weight: ${(props) => props.theme.mediumFontWeight};
+`;
+
+export default TitleH2;
