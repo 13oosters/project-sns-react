26cd5b655e3f76907f8931061c6302ebf8366d98
h12j21-star <jh06079@naver.com>
2022-12-19 16:55:57 +0900

    feat: useInput 컴포넌트 생성

---
+++ src/utils/useInput.js
@@ -0,0 +1,39 @@
+/* import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+ */
+/**
+ * useInput()은
+ *
+ * @params {object,string,object}
+ * @return {string} message
+ */
+
+/* export default function useInput(initialValue) {
+  const [value, setValue] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const API_END_POINT = "https://test.api.weniv.co.kr";
+
+  const request = async (url, option = {}) => {
+    try {
+      const fullUrl = `${API_END_POINT}${url}`;
+      const response = await axios(fullUrl, option);
+      response.post(API_END_POINT,{
+        console.log(1)
+      }).then
+      
+    } catch (error) {
+      setError(error);
+    }
+  };
+  
+
+  useEffect(()=>{
+    request()
+  },[])
+  return (
+    <div>useInput</div>
+  )
+}
+ */
