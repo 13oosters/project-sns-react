86f2f9e90797f3ede5f825b3c3102b6f1456cd57
h12j21-star <jh06079@naver.com>
2022-12-19 16:54:27 +0900

    design: form input padding값 위아래 변경 #2

--- src/components/style/form/LoginInput.js
+++ src/components/style/form/LoginInput.js
@@ -4,7 +4,7 @@ const LoginInput = styled.input`
   display: block;
   width: 100%;
   border-bottom: 0.1rem solid ${(props) => props.theme.lightColor};
-  padding: 0.8rem 0;
+  padding: 1rem 0.4rem 0.4rem 0;
   &::placeholder {
     color: ${(props) => props.theme.lightColor};
     font-size: ${(props) => props.theme.baseFontSize};
