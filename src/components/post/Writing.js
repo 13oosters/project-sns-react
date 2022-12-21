6d0a9ad03dc2a242900f64ea0c228ff12d9ea47b
h12j21-star <jh06079@naver.com>
2022-12-21 16:56:14 +0900

    Revert "design: Post 페이지 디자인 작업(comment,writing)완료 #6"
    This reverts commit 66094885b51f424ea70e6497d330a95442f94a87.

--- src/components/post/Writing.js
+++ src/components/post/Writing.js
@@ -1,57 +1,16 @@
 import React from "react";
-import styled from "styled-components";
 
 import profileImage from "../../assets/image/basic-profile-img-post.png";
 
-const WriteSection = styled.section`
-  display: flex;
-  justify-content: left;
-  align-items: center;
-  border-top: 1px solid ${(props) => props.theme.lightColor};
-  padding: 1.3rem 0;
-`;
-
-const UserImage = styled.img`
-  width: 3.6rem;
-  height: 3.6rem;
-`;
-const WritingForm = styled.form`
-  display: inherit;
-  justify-content: space-between;
-  width: 100%;
-  margin-left: 1.8rem;
-`;
-
-const WritingTextarea = styled.textarea`
-  width: 90%;
-  overflow: hidden;
-  margin: 1rem 0;
-  border: 0;
-  padding: 1.5rem 0 0 1rem;
-  &::placeholder {
-    color: #c4c4c4;
-  }
-  &:focus {
-    outline: 1px solid ${(props) => props.theme.darkLightColor};
-    border-radius: 0.4rem;
-  }
-`;
-
-const UpLoadButton = styled.button`
-  font-weight: ${(props) => props.theme.mediumFontWeight};
-  font-size: ${(props) => props.theme.baseFontSize};
-  color: #c4c4c4;
-`;
-
 export default function Writing() {
   return (
-    <WriteSection>
+    <section>
       <h4 class="sr-only">댓글 입력창</h4>
-      <UserImage src={profileImage} alt="사용자 프로필 사진" />
-      <WritingForm method="post">
-        <WritingTextarea required placeholder="댓글 입력하기.." />
-        <UpLoadButton type="submit">게시</UpLoadButton>
-      </WritingForm>
-    </WriteSection>
+      <img src={profileImage} alt="사용자 프로필 사진" />
+      <form method="post">
+        <textarea required placeholder="댓글 입력하기.." />
+        <button type="submit">게시</button>
+      </form>
+    </section>
   );
 }
