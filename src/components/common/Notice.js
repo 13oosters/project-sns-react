ca86afd2279a4080f5efc4540536f4905af3686d
yubmun <dlwnsduq23@gmail.com>
2022-12-21 12:09:23 +0900

    design: 홈페이지 구성 디자인 진행중 #7

--- src/components/common/Notice.js
+++ src/components/common/Notice.js
@@ -1,11 +1,36 @@
-import React from "react";
+import styled from "styled-components";
+import errorImage from "../../assets/image/404-img.png";
+import noticeImgage from "../../assets/image/logo-notice.png";
+import Button from "../style/Button";
 
-export default function Notice({ imgUrl, textContent, anchorContent }) {
+const NoticeSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  padding-top: 10rem;
+  min-height: 57rem;
+`
+const NoticeImage = styled.img`
+  width: 30.6rem;
+  height: 21.9rem;
+`
+
+const NoticeH3 = styled.h3`
+  font-size: ${(props) => props.theme.baseFontSize};
+  color: ${(props) => props.theme.darkLightColor};
+`
+
+const NoticeButton = styled(Button)`
+  width: 12rem;
+`
+
+export default function Notice({ type }) {
   return (
-    <section>
-      <img src={imgUrl} alt="#" />
-      <h3>{textContent}</h3>
-      <a href="#;">{anchorContent}</a>
-    </section>
+    <NoticeSection>
+      {type === "에러" ? <img src={errorImage} alt="#" /> : <NoticeImage src={noticeImgage} alt="#" />}
+      {type === "에러" ? <NoticeH3>페이지를 찾을 수 없습니다.</NoticeH3> : <NoticeH3>유저를 검색해 팔로우 해보세요!</NoticeH3>}
+      {type === "에러" ? <NoticeButton>이전 페이지</NoticeButton> : <NoticeButton>검색하기</NoticeButton>}
+    </NoticeSection>
   );
 }
---
+++ src/components/home/Feeds.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Cards from '../common/Cards'
+
+export default function Feeds() {
+  return (
+    <>
+      <Cards/>
+    </>
+  )
+}
