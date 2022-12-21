71dcddbf463209dc485ca8e42b5ed36d4fe11f00
h12j21-star <jh06079@naver.com>
2022-12-21 20:21:08 +0900

    design: header border-top추가 #6

--- src/components/style/Header.js
+++ src/components/style/Header.js
@@ -15,6 +15,7 @@ const HeaderLayout = css`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  border-bottom: 1px solid #dbdbdb;
   height: 4.8rem;
   padding: 0 1.6rem;
 `;
