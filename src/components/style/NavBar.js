11202f424cf8927490f92924e0c4b54fbbe9cb9a
yubmun <dlwnsduq23@gmail.com>
2022-12-21 16:39:11 +0900

    feat: 네브바 라우팅작업 #7

--- src/components/style/NavBar.js
+++ src/components/style/NavBar.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import homeImage from "../../assets/image/icon-home.png";
 import homeSelectImage from "../../assets/image/icon-home-fill.png"
 import searchImage from "../../assets/image/icon-search.png";
@@ -6,6 +7,14 @@ import searchSelectImage from "../../assets/image/icon-search-fill.png";
 import postImage from "../../assets/image/icon-post.png";
 import profileImage from "../../assets/image/icon-user.png";
 import profileSelectImage from "../../assets/image/icon-user-fill.png";
+
+const NavBarArticle = styled.article`
+  position: fixed;
+  bottom: 0;
+  z-index: 1;
+  width: 100%;
+  background-color: #ffffff;
+`
 
 const TabMenuUl = styled.ul`
   display: flex;
@@ -27,37 +36,39 @@ const SelectP = styled.p`
 `
 
 export default function NavBar({ type }) {
+  const navigate = useNavigate();
+
   return (
-    <article>
+    <NavBarArticle>
       <h3 className="sr-only">탭 메뉴</h3>
       <nav>
         <TabMenuUl>
           <li>
-            <TabMenuLink href="#;">
+            <TabMenuLink onClick={()=>{navigate("/")}}>
               {type === "홈" ? <img src={homeSelectImage} alt="홈으로 이동" /> : <img src={homeImage} alt="홈으로 이동" />}
               {type === "홈" ? <SelectP>홈</SelectP> : <p>홈</p>}
             </TabMenuLink>
           </li>
           <li>
-            <TabMenuLink href="#;">
+            <TabMenuLink onClick={()=>{navigate("/search")}}>
               {type === "검색" ? <img src={searchSelectImage} alt="검색으로 이동" /> : <img src={searchImage} alt="검색으로 이동" />}
               {type === "검색" ? <SelectP>검색</SelectP> : <p>검색</p>}
             </TabMenuLink>
           </li>
           <li>
-            <TabMenuLink href="#;">
+            <TabMenuLink onClick={()=>{navigate("/post")}}>
               {type === "게시물" ? <img src={postImage} alt="게시물 작성으로 이동" /> : <img src={postImage} alt="게시물 작성으로 이동" />}
               <p>게시물 작성</p>
             </TabMenuLink>
           </li>
           <li>
-            <TabMenuLink href="#;">
+            <TabMenuLink onClick={()=>{navigate("/profile")}}>
               {type === "프로필" ? <img src={profileSelectImage} alt="프로필로 이동" /> : <img src={profileImage} alt="프로필로 이동" />}
               {type === "프로필" ? <SelectP>프로필</SelectP> : <p>프로필</p>}
             </TabMenuLink>
           </li>
         </TabMenuUl>
       </nav>
-    </article>
+    </NavBarArticle>
   );
 }
