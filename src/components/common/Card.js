4ca927429e8204c8f43c32e41d890fa651763672
h12j21-star <jh06079@naver.com>
2022-12-21 13:38:30 +0900

    design: PostPage 디자인 진행중(Card) #6

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -1,19 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+
+import profileImage from "../../assets/image/basic-profile-img-post.png";
+import ModalButtonImage from "../../assets/image/icon-more-post.png";
+
+const CardTopDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1.5rem;
+`;
+
+const UserInfoDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const UserNameDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-left: 3rem;
+`;
+const UserNameStrong = styled.strong`
+  font-weight: ${(props) => props.theme.mediumFontWeight};
+  font-size: ${(props) => props.theme.baseFontSize};
+  margin-bottom: 0.2rem;
+`;
 
 export default function Card() {
   return (
-    <li>
-      <div>
-        <div>
-          <img src="#" alt="#" />
-          <div>
-            <strong>호박이</strong>
+    <li style={{ listStyle: "none" }}>
+      <CardTopDiv>
+        <UserInfoDiv>
+          <img src={profileImage} alt="유저 프로필 사진" />
+          <UserNameDiv>
+            <UserNameStrong>호박이</UserNameStrong>
             <p>hobak2</p>
-          </div>
-        </div>
+          </UserNameDiv>
+        </UserInfoDiv>
         {/* 자신의 프로필일 때만 */}
-        <button type="button">...</button>
-      </div>
+        <button type="button">
+          <img src={ModalButtonImage} alt="수정,삭제 모달창 버튼" />
+        </button>
+      </CardTopDiv>
       <img src="#" alt="#" />
       <ul>
         <li>
