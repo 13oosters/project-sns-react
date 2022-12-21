3eca0a46b18561b035ba4328c421162e303826fc
TommyKim97 <guana97@naver.com>
2022-12-21 15:12:58 +0900

    design: 프로필 페이지 마크업 진행중 #9

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+import Header from "../components/common/Header";
+import UserBio from "../components/profile/UserBio";
+import Cards from "../components/common/Cards";
+import NavBar from "../components/style/NavBar";
+
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <>
+      <h1 className="sr-only">프로필 페이지</h1>
+      <Header type="profile" />
+      <UserBio />
+      <Cards />
+      <NavBar type="프로필" />
+    </>
+  );
 }
