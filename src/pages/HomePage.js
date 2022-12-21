b160a492a584a901572f52d74ffdbc1c84667f6f
yubmun <dlwnsduq23@gmail.com>
2022-12-21 12:06:50 +0900

    design: 홈페이지 디자인 구성 진행중 #7

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Header from "../components/style/Header";
+// import Notice from "../components/common/Notice"
+import NavBar from "../components/style/NavBar";
+// import Cards from "../components/common/Cards";
+// import EmptyFeed from "../components/home/EmptyFeed";
+import Feeds from "../components/home/Feeds";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <section>
+      <Header type="logo"/>
+      {/* new */}
+      {/* <EmptyFeed/> */}
+      <Feeds/>
+      {/* feeds */}
+      {/* <Cards/> */}
+      <NavBar type="홈"/>
+    </section>
+  );
 }
