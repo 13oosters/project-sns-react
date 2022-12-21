996275d1d405c5e3d25ded383322f502ef16d4e0
yubmun <dlwnsduq23@gmail.com>
2022-12-21 12:09:59 +0900

    design: 헤더, 네브바 작업 진행중 #7

--- src/components/style/Header.js
+++ src/components/style/Header.js
@@ -6,10 +6,10 @@ import moreImage from "../../assets/image/icon-more-profile.png";
 import cancelImage from "../../assets/image/icon-cancel-search.png";
 
 /**
- * 최상위부모요소:
-  display:Flex;
-  justify-itesm: items-between;
-  padding 전부
+ * 최상위 부모요소:
+ * display: flex;
+ * justify-items: items-between;
+ * padding 전부
  */
 const HeaderLayout = css`
   display: flex;
@@ -17,17 +17,21 @@ const HeaderLayout = css`
   align-items: center;
   height: 4.8rem;
   padding: 0 1.6rem;
-`;
+  border-bottom: 0.5px solid ${(props) => props.theme.lightColor};
+`
+
 const HeaderUI = styled.div`
   ${HeaderLayout}
-`;
+`
+
 const HeaderInput = styled.input`
   display: block;
   height: 100%;
   background-color: #f2f2f2;
   padding: 0.7rem 4.3rem 0.7rem 1.6rem;
   border-radius: 3.2rem;
-`;
+`
+
 const SearchDiv = styled.div`
   position: relative;
   height: 100%;
@@ -38,20 +42,18 @@ const SearchDiv = styled.div`
     transform: translateY(-45%);
     right: 1.3rem;
   }
-`;
+`
 
 export default function Header({ type }) {
   const UI = {
     logo: (
       <HeaderUI>
-        <h1>
-          <img src={topLogoImage} alt="멍하냥" />
-        </h1>
+        <img src={topLogoImage} alt="멍하냥" />
       </HeaderUI>
     ),
     search: (
       <>
-        <h2 className="sr-only">검색창</h2>
+        <h2 class="sr-only">검색창</h2>
         <HeaderUI>
           <a href="#;">
             <img src={backImage} alt="뒤로 가기" />
@@ -59,7 +61,7 @@ export default function Header({ type }) {
           <SearchDiv>
             <HeaderInput placeholder="계정 검색" />
             <button type="button">
-              <img src={cancelImage} alt="취소버튼" />
+              <img src={cancelImage} alt="취소 버튼"/>
             </button>
           </SearchDiv>
         </HeaderUI>
@@ -71,7 +73,7 @@ export default function Header({ type }) {
           <img src={backImage} alt="뒤로 가기" />
         </a>
         <button type="button">
-          <img src={moreImage} alt="" />
+          <img src={moreImage} alt="더보기" />
         </button>
       </HeaderUI>
     ),
@@ -81,7 +83,7 @@ export default function Header({ type }) {
           <a href="#;">
             <img src={backImage} alt="뒤로 가기" />
           </a>
-          <h2 className="sr-only">Followers</h2>
+          <h2>Followers</h2>
         </HeaderUI>
       </>
     ),
--- src/components/style/NavBar.js
+++ src/components/style/NavBar.js
@@ -1,15 +1,18 @@
 import styled from "styled-components";
-import homeImg from "../../assets/image/icon-home.png";
-import searchImg from "../../assets/image/icon-search.png";
-import postImg from "../../assets/image/icon-post.png";
-import profileImg from "../../assets/image/icon-user.png";
+import homeImage from "../../assets/image/icon-home.png";
+import homeSelectImage from "../../assets/image/icon-home-fill.png"
+import searchImage from "../../assets/image/icon-search.png";
+import searchSelectImage from "../../assets/image/icon-search-fill.png";
+import postImage from "../../assets/image/icon-post.png";
+import profileImage from "../../assets/image/icon-user.png";
+import profileSelectImage from "../../assets/image/icon-user-fill.png";
 
 const TabMenuUl = styled.ul`
   display: flex;
   justify-content: space-between;
   border-top: 1px solid #dbdbdb;
   padding: 1.4rem 2.8rem 0.6rem;
-`;
+`
 
 const TabMenuLink = styled.a`
   display: flex;
@@ -17,10 +20,13 @@ const TabMenuLink = styled.a`
   align-items: center;
   gap: 0.4rem;
   line-height: 1.4;
-  color: #767676;
-`;
+  color: ${(props) => props.theme.darkLightColor};
+`
+const SelectP = styled.p`
+  color: ${(props) => props.theme.primaryColor};
+`
 
-export default function NavBar() {
+export default function NavBar({ type }) {
   return (
     <article>
       <h3 class="sr-only">탭 메뉴</h3>
@@ -28,26 +34,26 @@ export default function NavBar() {
         <TabMenuUl>
           <li>
             <TabMenuLink href="#;">
-              <img src={homeImg} alt="홈으로 이동" />
-              <p>홈</p>
+              {type === "홈" ? <img src={homeSelectImage} alt="홈으로 이동" /> : <img src={homeImage} alt="홈으로 이동" />}
+              {type === "홈" ? <SelectP>홈</SelectP> : <p>홈</p>}
             </TabMenuLink>
           </li>
           <li>
             <TabMenuLink href="#;">
-              <img src={searchImg} alt="검색으로 이동" />
-              <p>검색</p>
+              {type === "검색" ? <img src={searchSelectImage} alt="검색으로 이동" /> : <img src={searchImage} alt="검색으로 이동" />}
+              {type === "검색" ? <SelectP>검색</SelectP> : <p>검색</p>}
             </TabMenuLink>
           </li>
           <li>
             <TabMenuLink href="#;">
-              <img src={postImg} alt="게시물 작성으로 이동" />
+              {type === "게시물" ? <img src={postImage} alt="게시물 작성으로 이동" /> : <img src={postImage} alt="게시물 작성으로 이동" />}
               <p>게시물 작성</p>
             </TabMenuLink>
           </li>
           <li>
             <TabMenuLink href="#;">
-              <img src={profileImg} alt="프로필로 이동" />
-              <p>프로필</p>
+              {type === "프로필" ? <img src={profileSelectImage} alt="프로필로 이동" /> : <img src={profileImage} alt="프로필로 이동" />}
+              {type === "프로필" ? <SelectP>프로필</SelectP> : <p>프로필</p>}
             </TabMenuLink>
           </li>
         </TabMenuUl>
