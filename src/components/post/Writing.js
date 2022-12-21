1d523af707398ae108e9e5d665a04a0558d85322
h12j21-star <jh06079@naver.com>
2022-12-21 16:02:41 +0900

    Post 페이지 디자인 작업(wirting) 진행중 #6

--- src/components/post/Writing.js
+++ src/components/post/Writing.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import profileImage from "../../assets/image/basic-profile-img-post.png";
+
 export default function Writing() {
   return (
     <section>
       <h4 class="sr-only">댓글 입력창</h4>
-      <img src="" alt="gkdl" />
+      <img src={profileImage} alt="사용자 프로필 사진" />
       <form method="post">
         <textarea required placeholder="댓글 입력하기.." />
         <button type="submit">게시</button>
