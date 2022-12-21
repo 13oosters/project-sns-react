b9a7791c2a443a81c5c0b9d4c55eed96da14d857
h12j21-star <jh06079@naver.com>
2022-12-21 15:14:09 +0900

    Post 페이지 디자인 작업(Card)완료 #6

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 import profileImage from "../../assets/image/basic-profile-img-post.png";
 import ModalButtonImage from "../../assets/image/icon-more-post.png";
+import heartImage from "../../assets/image/icon-heart.png";
+import commentsImage from "../../assets/image/icon-Comments.png";
 
 const CardTopDiv = styled.div`
   display: flex;
@@ -26,6 +28,47 @@ const UserNameStrong = styled.strong`
   font-size: ${(props) => props.theme.baseFontSize};
   margin-bottom: 0.2rem;
 `;
+const UserIdP = styled.p`
+  display: block;
+  &::before {
+    content: "@";
+  }
+`;
+
+const PostImage = styled.img`
+  width: 100%;
+  height: 23rem;
+  // 임시 border
+  border: 1px solid black;
+`;
+
+const ButtonUl = styled.ul`
+  display: flex;
+  padding: 0 2.4rem;
+  gap: 2rem;
+`;
+const Buttonli = styled.li`
+  display: inherit;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const NumberSpan = styled.span``;
+const PostCommentP = styled.p`
+  text-align: left;
+  margin-top: 0.8rem;
+  padding: 0 2.4rem;
+  font-weight: ${(props) => props.theme.mediumFontWeight};
+  font-size: ${(props) => props.theme.baseFontSize};
+`;
+
+const PostTime = styled.time`
+  display: block;
+  margin-top: 1rem;
+  padding: 0 2.4rem;
+  font-size: ${(props) => props.theme.xSmallFontSize};
+`;
 
 export default function Card() {
   return (
@@ -35,7 +78,7 @@ export default function Card() {
           <img src={profileImage} alt="유저 프로필 사진" />
           <UserNameDiv>
             <UserNameStrong>호박이</UserNameStrong>
-            <p>hobak2</p>
+            <UserIdP>hobak2</UserIdP>
           </UserNameDiv>
         </UserInfoDiv>
         {/* 자신의 프로필일 때만 */}
@@ -43,25 +86,48 @@ export default function Card() {
           <img src={ModalButtonImage} alt="수정,삭제 모달창 버튼" />
         </button>
       </CardTopDiv>
-      <img src="#" alt="#" />
-      <ul>
-        <li>
-          <button type="button">
-            <img src="#" alt="#" />
+      <PostImage src="#" alt="#" />
+      <ButtonUl>
+        <Buttonli>
+          <button type="button" style={{ padding: 0 }}>
+            <img src={heartImage} alt="게시글 좋아요 버튼" />
           </button>
-          <span>59</span>
-        </li>
-        <li>
+          <NumberSpan>59</NumberSpan>
+        </Buttonli>
+        <Buttonli>
           <a href="#;">
-            <img src="3" alt="#" />
+            <img src={commentsImage} alt="게시글 댓글 버튼" />
           </a>
-          <span>3</span>
-        </li>
-      </ul>
-      <p>집사의 코딩을 방해하는 나, 제법 귀여울지도?</p>
-      <time datetime="2022-12-05">
+          <NumberSpan>3</NumberSpan>
+        </Buttonli>
+      </ButtonUl>
+      <PostCommentP>
+        집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나,
+        제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의
+        코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을
+        방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을
+        방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을
+        방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을
+        방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을
+        방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을
+        방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을
+        방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을
+        방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도? 집사의 코딩을 방해하는 나, 제법 귀여울지도? 집사의 코딩을
+        방해하는 나, 제법 귀여울지도? 집사의 코딩을 방해하는 나, 제법
+        귀여울지도?
+      </PostCommentP>
+      <PostTime datetime="2022-12-05">
         <span>2022</span> <span>12</span> <span>5</span>
-      </time>
+      </PostTime>
     </li>
   );
 }
