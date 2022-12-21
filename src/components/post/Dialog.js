16f773f64110f27656c7006433d7e7a0b3278391
h12j21-star <jh06079@naver.com>
2022-12-21 16:54:39 +0900

    design: Dialog 디자인 완료 #6

--- src/components/post/Dialog.js
+++ src/components/post/Dialog.js
@@ -1,15 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
 import Comments from "./Comments";
 import Writing from "./Writing";
-import PageLayout from "../style/PageLayout";
+
+const DialogSection = styled.section`
+  padding: 10rem 1.6rem 0;
+`;
 
 export default function Dialog() {
   return (
-    <PageLayout paddingValue={"1.6"}>
+    <DialogSection>
       <h3 class="sr-only">댓글 창</h3>
       <Comments />
       <Writing />
-    </PageLayout>
+    </DialogSection>
   );
 }
