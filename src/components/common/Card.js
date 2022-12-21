31c1376137574d757b8d8cff6ef8b46bacccd6ca
yubmun <dlwnsduq23@gmail.com>
2022-12-21 16:40:35 +0900

    design: 카드, 피드 디자인 진행중 #7

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -1,38 +1,99 @@
-import React from "react";
+import styled, { css } from "styled-components";
+import defaultImage from "../../assets/image/basic-profile-img-post.png";
+import moreImage from "../../assets/image/icon-more-post.png";
+import homeTestImage from "../../assets/image/home-test.png";
+import heartImage from "../../assets/image/icon-heart.png";
+import commentImage from "../../assets/image/icon-comment.png";
+
+const smallFont = css`
+  font-size: ${(props) => props.theme.smallFontSize};
+  color: ${(props) => props.theme.darkLightColor};
+`
+
+const CardHeaderDiv = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 1.5rem;
+  gap: 1.5rem;
+`
+
+const CardHeaderStrong = styled.strong`
+  font-size: ${(props) => props.theme.baseFontSize};
+  font-weight: 500;
+`
+const CardHeaderP = styled.p`
+  ${smallFont}
+  margin-top: 0.2rem;
+  &::before{
+    content: '@ ';
+  }
+`
+const CardHeaderButton = styled.button`
+  margin-left: auto;
+  padding: 0;
+`
+const CardBodyUl = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  padding: 0.8rem;
+`
+const CardBodySpan = styled.span`
+  ${smallFont}
+  vertical-align: middle;
+  margin-left: 0.9rem;
+`
+const CardBodyImage = styled.img`
+  vertical-align: middle;
+`
+const CardBodyP = styled.p`
+  font-size: ${(props) => props.theme.baseFontSize};
+  padding: 0 1.3rem;
+`
+const CardBodyTime = styled.time`
+  ${smallFont};
+  display: flex;
+  gap: 0.5rem;
+  padding: 1.3rem;
+`
 
 export default function Card() {
   return (
     <li>
-      <div>
+      <CardHeaderDiv>
+          <img src={defaultImage} alt="#" />
         <div>
-          <img src="#" alt="#" />
           <div>
-            <strong>호박이</strong>
-            <p>hobak2</p>
+            <CardHeaderStrong>호박이</CardHeaderStrong>
+            <CardHeaderP>hobak2</CardHeaderP>
           </div>
         </div>
-        {/* 자신의 프로필일 때만 */}
-        <button type="button">...</button>
-      </div>
-      <img src="#" alt="#" />
-      <ul>
+        {/* 자신의 게시글일 때만 */}
+        <CardHeaderButton type="button">
+          <img src={moreImage} alt="설정" />
+        </CardHeaderButton>
+      </CardHeaderDiv>
+      <img src={homeTestImage} alt="#" style={{width: "100%"}}/>
+      <CardBodyUl>
         <li>
           <button type="button">
-            <img src="#" alt="#" />
+            <CardBodyImage src={heartImage} alt="#" />
           </button>
-          <span>59</span>
+          <CardBodySpan>59</CardBodySpan>
         </li>
         <li>
           <a href="#;">
-            <img src="3" alt="#" />
+            <CardBodyImage src={commentImage} alt="#" />
           </a>
-          <span>3</span>
+          <CardBodySpan style={{transform: "translateY(-5%)"}}>3</CardBodySpan>
         </li>
-      </ul>
-      <p>집사의 코딩을 방해하는 나, 제법 귀여울지도?</p>
-      <time dateTime="2022-12-05">
-        <span>2022</span> <span>12</span> <span>5</span>
-      </time>
+      </CardBodyUl>
+      <CardBodyP>집사의 코딩을 방해하는 나, 제법 귀여울지도?</CardBodyP>
+      <CardBodyTime dateTime="2022-12-05">
+        <span>2022년</span>
+        <span>12월</span>
+        <span>5일</span>
+      </CardBodyTime>
     </li>
   );
 }
--- src/components/home/Feeds.js
+++ src/components/home/Feeds.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import Cards from '../common/Cards'
+import styled from "styled-components";
+import Cards from '../common/Cards';
+
+const FeedsDiv = styled.div`
+  height: 57rem;
+  overflow: scroll;
+  margin-top: 4rem;
+  margin-bottom: 6rem;
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`
 
 export default function Feeds() {
   return (
-    <>
+    <FeedsDiv>
       <Cards/>
-    </>
+    </FeedsDiv>
   )
 }
