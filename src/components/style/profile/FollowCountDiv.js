055ca9afd59d46aee07f2090885ec416a250df2d
TommyKim97 <guana97@naver.com>
2022-12-22 22:24:33 +0900

    design: 프로필 페이지 팔로우, 팔로잉 링크 스타일 #9

---
+++ src/components/style/profile/FollowCountDiv.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const FollowCountDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+export default FollowCountDiv;
