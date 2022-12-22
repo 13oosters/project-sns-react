663aad79396bf9279352bd9e1d66127db72bf3e0
TommyKim97 <guana97@naver.com>
2022-12-22 22:27:57 +0900

    design: 프로필 페이지 팔로우,팔로잉 링크 스타일 #9

--- src/components/profile/FollowerCount.js
+++ src/components/profile/FollowerCount.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import FollowCountDiv from "../style/profile/FollowCountDiv";
+import FollowCountP from "../style/profile/FollowCountP";
+import FollowCountSpan from "../style/profile/FollowCountSpan";
 
 export default function FollowerCount() {
   return (
-    <div>
-      <strong>FollowerCount</strong>
-      <p>followers</p>
-    </div>
+    <FollowCountDiv>
+      <Link>
+        <FollowCountP>1209</FollowCountP>
+        <FollowCountSpan>followers</FollowCountSpan>
+      </Link>
+    </FollowCountDiv>
   );
 }
--- src/components/profile/FollowingCount.js
+++ src/components/profile/FollowingCount.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import FollowCountDiv from "../style/profile/FollowCountDiv";
+import FollowCountP from "../style/profile/FollowCountP";
+import FollowCountSpan from "../style/profile/FollowCountSpan";
 
 export default function FollowingCount() {
   return (
-    <div>
-      <strong>FollowingCount</strong>
-      <p>Followings</p>
-    </div>
+    <FollowCountDiv>
+      <Link>
+        <FollowCountP>1290</FollowCountP>
+        <FollowCountSpan>followings</FollowCountSpan>
+      </Link>
+    </FollowCountDiv>
   );
 }
