fdc11ff563c9cf20c3d2b29832cd476d84e04954
yubmun <dlwnsduq23@gmail.com>
2022-12-22 11:17:32 +0900

    feat: 버튼눌렀을때 이전페이지, 검색페이지로 이동 #7

--- src/components/common/Notice.js
+++ src/components/common/Notice.js
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import errorImage from "../../assets/image/404-img.png";
 import noticeImgage from "../../assets/image/logo-notice.png";
@@ -26,11 +27,13 @@ const NoticeButton = styled(Button)`
 `
 
 export default function Notice({ type }) {
+  const navigate = useNavigate();
+
   return (
     <NoticeSection>
       {type === "에러" ? <img src={errorImage} alt="#" /> : <NoticeImage src={noticeImgage} alt="#" />}
       {type === "에러" ? <NoticeH3>페이지를 찾을 수 없습니다.</NoticeH3> : <NoticeH3>유저를 검색해 팔로우 해보세요!</NoticeH3>}
-      {type === "에러" ? <NoticeButton>이전 페이지</NoticeButton> : <NoticeButton>검색하기</NoticeButton>}
+      {type === "에러" ? <NoticeButton onClick={()=>{navigate(-1)}}>이전 페이지</NoticeButton> : <NoticeButton onClick={()=>{navigate("/search")}}>검색하기</NoticeButton>}
     </NoticeSection>
   );
 }
