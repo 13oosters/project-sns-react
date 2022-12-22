8a7caa7ca80090d57ca6e39c9f0d00944236d240
TommyKim97 <guana97@naver.com>
2022-12-23 00:24:17 +0900

    design: 프로필 페이지 마크업 및 스타일 작업 #9

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-import Header from "../components/common/Header";
-import UserBio from "../components/profile/UserBio";
+import Header from "../components/style/Header";
 import Cards from "../components/common/Cards";
 import NavBar from "../components/style/NavBar";
+import ProfileInformation from "../components/profile/ProfileInformation";
+import SortButtons from "../components/profile/SortButtons";
 
 export default function ProfilePage() {
   return (
     <section>
       <h1 class="sr-only">프로필 페이지</h1>
       <Header type="profile" />
-      <UserBio />
+      <ProfileInformation />
+      <SortButtons />
       <Cards />
       <NavBar />
     </section>
