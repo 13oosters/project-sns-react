18b8b466059c6ebebe25cd7c88e5347d8f5d0e25
h12j21-star <jh06079@naver.com>
2022-12-24 00:46:08 +0900

    feat: 댓글 추가 작업 #6

--- src/components/post/Detail.js
+++ src/components/post/Detail.js
@@ -3,15 +3,16 @@ import React from "react";
 import Card from "../common/Card";
 import Dialog from "../post/Dialog";
 
-export default function Detail({ setIsModal, postStoreData }) {
+export default function Detail({ setIsModal, postStoreData, id }) {
   /** 유저정보는 공통 card에서 가져오기..? */
   const { post } = { ...postStoreData };
+  const { comments } = { ...postStoreData };
 
   return (
     <section>
       <h2 class="sr-only">게시글</h2>
       <Card setIsModal={setIsModal} post={post} />
-      <Dialog />
+      <Dialog comments={comments} id={id} />
     </section>
   );
 }
