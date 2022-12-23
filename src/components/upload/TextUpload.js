295c1f9d046294766874b72dc5e348c43760748c
Jxxunnn <roof2586@naver.com>
2022-12-23 23:00:19 +0900

    design: TextUpload 컴포넌트 #14

--- src/components/upload/TextUpload.js
+++ src/components/upload/TextUpload.js
@@ -1,5 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+
+const UploadTextarea = styled.textarea`
+  display: block;
+  padding: 3.2rem 3.5rem 3.2rem 7rem;
+  border: 0;
+  width: 100%;
+  //  height: calc(100%-210rem);
+  &::-webkit-scrollbar {
+    height: 0.5rem;
+    width: 0.5rem;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #dbdbdb;
+    border-radius: 10px;
+  }
+
+  &::placeholder {
+    color: #c4c4c4;
+    font-size: ${(props) => props.theme.baseFontSize};
+  }
+  &:focus {
+    border-radius: 1rem;
+    outline: none;
+  }
+`;
 
 export default function TextUpload() {
-  return <textarea required placeholder="게시글 입력하기.."></textarea>;
+  return (
+    <UploadTextarea
+      required
+      rows="5"
+      cols="33"
+      placeholder="게시글 입력하기.."
+    ></UploadTextarea>
+  );
 }
