6f6faee8fdb95a9501579f555086284b7a36b84b
soyoung.kim <101047198+TommyKim97@users.noreply.github.com>
2022-12-23 17:31:04 +0900

    Feat/profile (#15)

---
+++ src/components/profile/GridCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function GridCard() {
+  return (
+    <div>
+      <img src="" alt="" />
+    </div>
+  );
+}
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+import Header from "../components/style/Header";
+import Cards from "../components/common/Cards";
+import NavBar from "../components/style/NavBar";
+import ProfileInformation from "../components/profile/ProfileInformation";
+import SortButtons from "../components/profile/SortButtons";
+
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <section>
+      <h1 class="sr-only">프로필 페이지</h1>
+      <Header type="profile" />
+      <ProfileInformation />
+      <SortButtons />
+      <Cards />
+      <NavBar />
+    </section>
+  );
 }
