19c482b4223ba3c499c8338f4514ad00b146dbe1
Jxxunnn <roof2586@naver.com>
2022-12-23 23:00:30 +0900

    design: UploadPage 컴포넌트 #14

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Posting from "../components/upload/Posting";
 
 import PageLayout from "../components/style/PageLayout";
 
 export default function UploadPage() {
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    accountname: "",
+    intro: "",
+    image: "https://mandarin.api.weniv.co.kr/Ellipse.png",
+  });
+
   return (
     <PageLayout paddingValue={0}>
       <h1 className="sr-only">게시글 업로드 페이지</h1>
-      <Posting />
+      <Posting userData={userData} setUserData={setUserData} />
     </PageLayout>
   );
 }
