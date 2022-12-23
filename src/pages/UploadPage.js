3e16b86d231cb423c23854e955ec10009e5ec405
Jxxunnn <roof2586@naver.com>
2022-12-23 20:04:36 +0900

    feat: UploadPage 컴포넌트 생성 #14

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+import Posting from "../components/upload/Posting";
+
+import PageLayout from "../components/style/PageLayout";
+
 export default function UploadPage() {
-  return <div>UploadPage</div>;
+  return (
+    <PageLayout paddingValue={0}>
+      <h1 className="sr-only">게시글 업로드 페이지</h1>
+      <Posting />
+    </PageLayout>
+  );
 }
