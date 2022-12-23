a4406fdd3035b8b67cc22bb24d7aac4d00b3fdf9
yubmun <dlwnsduq23@gmail.com>
2022-12-24 00:46:05 +0900

    feat: 피드 데이터 수신 및 카드 props 전달 #7

--- src/components/common/Cards.js
+++ src/components/common/Cards.js
@@ -3,12 +3,16 @@ import Card from "./Card";
 
 export default function Cards({feed}) {
 
+  const {posts} = {...feed};
+
+  // console.log(posts);
 
   return (
     <section>
       <h3 className="sr-only">게시글</h3>
       <ol reversed>
-        <Card feed={feed}/>
+        {posts ? posts.map((post) => <Card post={post}/>) : <></>}
+        {/* <Card post={feed}/> */}
         {/* <Card />
         <Card />
         <Card /> */}
