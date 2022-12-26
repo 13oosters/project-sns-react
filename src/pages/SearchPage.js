9c9b813b7caca5ee8f333a0776fb489d1983c4a5
Jxxunnn <roof2586@naver.com>
2022-12-26 21:41:10 +0900

    feat: useTransition 최적화 #21

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState, useTransition } from "react";
 
 import Header from "../components/style/Header";
 import Results from "../components/search/Results";
 import NavBar from "../components/style/NavBar";
+import PageLayout from "../components/style/PageLayout";
 
 export default function SearchPage() {
+  const [keyword, setKeyword] = useState(undefined);
+  const [isPending, startTransition] = useTransition();
+
   return (
-    <section>
-      <h1 class="sr-only">검색 페이지</h1>
-      <Header type="search" />
-      <Results />
+    <PageLayout paddingValue={0}>
+      <h1 className="sr-only">검색 페이지</h1>
+      <Header
+        setKeyword={setKeyword}
+        startTransition={startTransition}
+        type="search"
+      />
+      <Results keyword={keyword} isPending={isPending} />
       <NavBar type="검색" />
-    </section>
+    </PageLayout>
   );
 }
