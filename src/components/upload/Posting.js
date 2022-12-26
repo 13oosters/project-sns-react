7cb2c85f6026484922a9b2b30d3a17aae59b79ba
Jxxunnn <roof2586@naver.com>
2022-12-26 10:46:02 +0900

    feat: 게시글 업로드 기능 구현 #14

--- src/components/upload/Posting.js
+++ src/components/upload/Posting.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 import ImageUpload from "./ImageUpload";
 import TextUpload from "./TextUpload";
 import Button from "../style/Button";
 
 import PageLayout from "../style/PageLayout";
+import API from "../../utils/api";
 
 const UploadButton = styled(Button)`
   margin: 2rem 2rem 2rem auto;
@@ -15,11 +17,43 @@ const UploadButton = styled(Button)`
 `;
 
 export default function Posting({ userData, setUserData }) {
+  const [imageData, setImageData] = useState([]);
+  const navigate = useNavigate();
+
+  const uploadPost = async (e) => {
+    e.preventDefault();
+    try {
+      await API.post(
+        "/post",
+        {
+          post: {
+            content: e.target[2].value,
+            image: imageData.join(", "),
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-type": "application/json",
+          },
+        },
+      );
+    } catch (error) {
+      throw new Error(error);
+    } finally {
+      navigate(`/${userData.accountname}`);
+    }
+  };
+
   return (
     <PageLayout paddingValue={0}>
       <h2 className="sr-only">게시글 작성</h2>
-      <form>
-        <ImageUpload userData={userData} />
+      <form onSubmit={uploadPost}>
+        <ImageUpload
+          userData={userData}
+          imageData={imageData}
+          setImageData={setImageData}
+        />
         <TextUpload />
         <UploadButton type="submit">업로드</UploadButton>
       </form>
