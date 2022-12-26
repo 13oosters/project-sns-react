9b582b4739cc82715308db5664967cd9dc19492e
TommyKim97 <guana97@naver.com>
2022-12-26 13:28:11 +0900

    feat: 팔로워 리스트 페이지 api 연결 #20

--- src/pages/FollowersPage.js
+++ src/pages/FollowersPage.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { BASE_URL } from "../utils/api";
+
 import FollowerCards from "../components/follow/FollowerCards";
 import Header from "../components/style/Header";
+import NavBar from "../components/style/NavBar";
+
+const FollowerWrap = styled.div`
+  width: 100%;
+`;
 
 export default function FollowersPage() {
+  const [followerList, setFollowerList] = useState([]);
+  const token = localStorage.getItem("token");
+  const accountname = localStorage.getItem("accountname");
+  const url = `${BASE_URL}/profile/${accountname}/follower`;
+  const getFollowerList = () => {
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
+        },
+      })
+      .then((response) => {
+        setFollowerList(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getFollowerList();
+  }, []);
+
   return (
-    <>
+    <FollowerWrap>
       <Header type="followers" />
-      <FollowerCards />
-    </>
+      <FollowerCards followerList={followerList} />
+      <NavBar />
+    </FollowerWrap>
   );
 }
