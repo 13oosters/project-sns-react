163444cf905bc8bf593a0ba90cb56c2eedc6979c
yubmun <dlwnsduq23@gmail.com>
2022-12-30 23:06:37 +0900

    design: 공통작업 로고 #38

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -4,9 +4,21 @@ import styled from "styled-components";
 import logoImage from "../../assets/image/logo-login.png";
 
 export const LogoImage = styled.img`
-  margin-bottom: 9.4rem;
+  width: 100%;
+  /* margin: 7.3rem 0 9.4rem 0; */
+  transform: scale(0.8);
+`;
+
+const LogoImageDiv = styled.div`
+//  padding: 7.3rem 0 9.4rem 0;
+  display: flex;
+  align-items: center;
 `;
 
 export default function Logo() {
-  return <LogoImage src={logoImage} alt="멍하냥" />;
+  return (
+    <LogoImageDiv>
+      <LogoImage src={logoImage} alt="멍하냥" />
+    </LogoImageDiv>
+  );
 }
