1928ee8a3e7aa40855aebbc389529106eb719786
leejungeun <86228307+Jxxunnn@users.noreply.github.com>
2022-12-31 16:12:39 +0900

    feat: 로그인, 홈 화면 스플래시 스크린 기능 구현 #2 (#42)

--- src/components/common/SplashScreen.js
+++ src/components/common/SplashScreen.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
 import splashImage from "../../assets/image/logo-splash.png";
 
 const SplashArticle = styled.article`
   display: flex;
-  position: absolute;
   justify-content: center;
   align-items: center;
-  height: 100%;
-  width: 100%;
-  background-color: ${(props) => props.theme.primaryColor};
+  margin: 0 auto;
+  height: 100vh;
+  max-width: 500px;
+  background-color: ${(props) => props.theme.secondaryColor};
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 `;
 
-export default function SplashScreen() {
+export default function SplashScreen({ setDidSplashScreenMount }) {
+  const removeSplashScreen = () => {
+    setDidSplashScreenMount(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", removeSplashScreen);
+    return () => {
+      removeEventListener("keydown", removeSplashScreen);
+    };
+  });
   return (
-    <SplashArticle>
+    <SplashArticle onClick={removeSplashScreen}>
       <h1 className="sr-only">멍하냥 로딩 화면</h1>
       <img src={splashImage} alt="멍하냥 로고" />
     </SplashArticle>
