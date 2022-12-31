5580325e71f24fe4980fb15d36e27fcdd2b1a066
Jxxunnn <roof2586@naver.com>
2022-12-31 16:44:31 +0900

    feat: 이미지 src 에러시 기본 이미지 대체 기능 #21

--- src/components/search/Result.js
+++ src/components/search/Result.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
-import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
 import defaultImage from "../../assets/image/basic-profile-img-post.png";
 
@@ -42,32 +42,8 @@ const SearchP = styled.p`
 `;
 
 export default function Result({ image, username, accountname, keyword }) {
-  const [isFailed, setIsFailed] = useState(false);
-  const [imageUrl, setImageUrl] = useState("");
   const navigate = useNavigate();
 
-  // console.log(isFailed)
-
-  /*   const checkImage = async () => {
-    try {
-      const res = await axios.get(`https://mandarin.api.weniv.co.kr/${image}`);
-      const status = await res.data?.status;
-
-      if (status) {
-        setIsFailed(true);
-        //       console.log(isFailed)
-      }
-    } catch (e) {}
-  };
- */
-  /*  useEffect(() => {
-    checkImage();
-  }, []);
-  if (isFailed) {
-    console.log(url[0])
-      return defaultImage;
-    }
- */
   const refineImageData = (filename) => {
     const url = filename.split(",");
     const [, , base] = filename.split("/");
@@ -90,18 +66,19 @@ export default function Result({ image, username, accountname, keyword }) {
     } else if (base === "market-52.herokuapp.com") {
       return defaultImage;
     } else if (url[0].includes("https://mandarin.api.weniv.co.kr/")) {
-      console.log(1);
       if (isNaN(url[0][33])) {
         return defaultImage;
       }
-      console.log(2);
       return filename;
     } else if (isNaN(url[0][0])) {
-      console.log(3);
       return `https://mandarin.api.weniv.co.kr/${url[0]}`;
     } else {
       return defaultImage;
     }
+  };
+
+  const noImage = (e) => {
+    e.target.src = defaultImage;
   };
 
   return (
@@ -110,7 +87,11 @@ export default function Result({ image, username, accountname, keyword }) {
         navigate(`/${accountname}`);
       }}
     >
-      <SearchImg src={refineImageData(image)} alt={username} />
+      <SearchImg
+        src={refineImageData(image)}
+        onError={noImage}
+        alt={username}
+      />
       <div>
         <SearchStrong>{username}</SearchStrong>
         <SearchP>{accountname}</SearchP>
