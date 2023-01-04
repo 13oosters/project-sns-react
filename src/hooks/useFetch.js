f703237ed81e13b133d2c77b2384433c76a07957
Jxxunnn <roof2586@naver.com>
2023-01-04 13:48:13 +0900

    fix: 쿼리 스트링이 빈 값일 경우 useFetch 함수가 빈 배열을 리턴하도록 수정

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,12 +6,18 @@ export default function useFetch(path, initialType, dependency = []) {
   const [data, setData] = useState(initialType);
 
   useEffect(() => {
-    API(`${path}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-        "Content-type": "application/json",
-      },
-    }).then((res) => setData(res.data));
+    const [, query] = path.split("=")[1];
+
+    if (query) {
+      API(`${path}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-type": "application/json",
+        },
+      }).then((res) => setData(res.data));
+    } else {
+      setData(initialType);
+    }
   }, dependency);
 
   return [data];
